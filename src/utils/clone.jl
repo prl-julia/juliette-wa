4fcd03f246fae8fba720a179e672445889010a56
gelinasj <gelinas.j@husky.neu.edu>
2020-07-15 17:39:56 -0400

    removing newlen

--- src/utils/clone.jl
+++ src/utils/clone.jl
@@ -1,4 +1,3 @@
-
 # Clones all the repositories in the source file (where all the repo URL's are
 # on their own line) into the given destination directory
 function clone(source :: String, destination :: String)
