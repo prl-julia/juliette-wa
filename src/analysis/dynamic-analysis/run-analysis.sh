c3457d401835a43d1e3b8043afb92a4e39ce16c0
gelinasj <gelinas.j@husky.neu.edu>
2020-07-22 11:30:42 -0400

    updating file name

--- src/analysis/dynamic-analysis/run-analysis.sh
+++ src/analysis/dynamic-analysis/run-analysis.sh
@@ -4,7 +4,7 @@ PACKAGES_TO_ANALYZE=("JuMP" "Genie" "Zygote" "TensorFlow" "MLJ" "UnicodePlots" "
 
 for pkg in ${PACKAGES_TO_ANALYZE[@]}
 do
-    ~/AppData/Local/Programs/Julia/Julia-1.5.0/bin/julia.exe analyze-packages.jl $pkg &
+    ~/AppData/Local/Programs/Julia/Julia-1.5.0/bin/julia.exe main.jl $pkg &
 done
 
 # Packages with issue in Pkg.test(...):
