d1f05a3a3f328c16415cb9f8b530698af551285e
gelinasj <gelinas.j@husky.neu.edu>
2020-08-14 12:30:44 -0400

    removing depot path change

--- src/analysis/dynamic-analysis/main.jl
+++ src/analysis/dynamic-analysis/main.jl
@@ -6,9 +6,9 @@ function analyzePkg(pkg :: String)
     ENV["DYNAMIC_ANALYSIS_DIR"] = pwd()
     ENV["DYNAMIC_ANALYSIS_PACKAGE_NAME"] = pkg
     ENV["OUTPUT_DIR"] = "$(ENV["DYNAMIC_ANALYSIS_DIR"])/package-data/$(ENV["DYNAMIC_ANALYSIS_PACKAGE_NAME"])"
-    DEPOT_PATH[1] = "$(ENV["OUTPUT_DIR"])/env"
+    # DEPOT_PATH[1] = "$(ENV["OUTPUT_DIR"])/env"
     try mkdir(ENV["OUTPUT_DIR"]) catch e end
-    try mkdir(DEPOT_PATH[1]) catch e end
+    # try mkdir(DEPOT_PATH[1]) catch e end
     Pkg.add(pkg)
     Pkg.test(pkg)
 end
