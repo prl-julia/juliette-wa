599fc6c53831c17433cbf9c64975ed2f5d1abf3a
gelinasj <gelinas.j@husky.neu.edu>
2020-08-18 18:26:51 -0400

    adding bash script

---
+++ src/analysis/dynamic-analysis/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PACKAGES_TO_ANALYZE=(
+    "Genie", "Documenter", "Plots", "CxxWrap", "IJulia", "PyCall", "Weave",
+    "BenchmarkTools", "Pluto", "Literate", "Revise", "Dagger", "Unitful",
+    "ModelingToolkit", "Cxx", "Franklin"
+)
+
+for pkg in ${PACKAGES_TO_ANALYZE[@]}
+do
+    julia main.jl $pkg
+done
