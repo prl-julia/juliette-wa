9819daf184485d517c5c7c20d937a7ff71bcbeaf
Your Name <gelinas.j@northeastern.edu>
2020-08-19 01:13:05 +0200

    adding sequential package analysis

--- src/analysis/dynamic-analysis/run.sh
+++ src/analysis/dynamic-analysis/run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 PACKAGES_TO_ANALYZE=(
-    "Genie", "Documenter", "Plots", "CxxWrap", "IJulia", "PyCall", "Weave",
-    "BenchmarkTools", "Pluto", "Literate", "Revise", "Dagger", "Unitful",
-    "ModelingToolkit", "Cxx", "Franklin"
+    "Genie"
+    "Documenter"
+    "Plots"
+    "CxxWrap"
+    "IJulia"
+    "PyCall"
+    "Weave"
+    "BenchmarkTools"
+    "Pluto"
+    "Literate"
+    "Revise"
+    "Dagger"
+    "Unitful"
+    "ModelingToolkit"
+    "Cxx"
+    "Franklin"
 )
 
 for pkg in ${PACKAGES_TO_ANALYZE[@]}
 do
-    julia main.jl $pkg
+    julia main.jl $pkg 
 done
