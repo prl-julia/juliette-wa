c6979300b9fd9920bf7de755045e7579de1399bb
gelinasj <gelinas.j@husky.neu.edu>
2020-08-18 17:24:58 -0400

    adding versioning

--- src/analysis/dynamic-analysis/main.jl
+++ src/analysis/dynamic-analysis/main.jl
@@ -3,14 +3,12 @@ using Pkg
 
 # Test package with overriden functions
 function analyzePkg(pkgName :: String, pkgVersion :: Union{String, Nothing})
-    ENV["DYNAMIC_ANALYSIS_DIR"] = pwd()
-    ENV["DYNAMIC_ANALYSIS_PACKAGE_NAME"] = pkgName
-    ENV["OUTPUT_DIR"] = "$(ENV["DYNAMIC_ANALYSIS_DIR"])/package-data/$(ENV["DYNAMIC_ANALYSIS_PACKAGE_NAME"])"
-    # DEPOT_PATH[1] = "$(ENV["OUTPUT_DIR"])/env"
-    try mkdir(ENV["OUTPUT_DIR"]) catch e end
-    # try mkdir(DEPOT_PATH[1]) catch e end
     pkg = pkgVersion == nothing ? pkgName : Pkg.PackageSpec(;name=pkgName, version=pkgVersion)
     Pkg.add(pkg)
+    ENV["DYNAMIC_ANALYSIS_DIR"] = pwd()
+    ENV["DYNAMIC_ANALYSIS_PACKAGE_NAME"] = pkgName
+    ENV["OUTPUT_DIR"] = "$(ENV["DYNAMIC_ANALYSIS_DIR"])/package-data/$(pkgName)-$(Pkg.installed()[pkgName])"
+    try mkdir(ENV["OUTPUT_DIR"]) catch e end
     Pkg.test(pkg)
 end
 
