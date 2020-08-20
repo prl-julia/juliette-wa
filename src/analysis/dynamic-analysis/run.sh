64511325cfff7bec888741409dc125faecf81022
gelinasj <gelinas.j@husky.neu.edu>
2020-08-20 13:08:19 -0400

    removing old analyzed data

--- src/analysis/dynamic-analysis/run.sh
+++ src/analysis/dynamic-analysis/run.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-PACKAGES_TO_ANALYZE=("Weave")
-
-# PACKAGES_TO_ANALYZE=(
-#     "Genie"
-#     "Documenter"
-#     "Plots"
-#     "CxxWrap"
-#     "IJulia"
-#     "PyCall"
-#     "Weave"
-#     "BenchmarkTools"
-#     "Pluto"
-#     "Literate"
-#     "Revise"
-#     "Dagger"
-#     "Unitful"
-#     "ModelingToolkit"
-#     "Cxx"
-#     "Franklin"
-# )
+PACKAGES_TO_ANALYZE=(
+    "Genie"
+    "Documenter"
+    "Plots"
+    "CxxWrap"
+    "IJulia"
+    "PyCall"
+    "Weave"
+    "BenchmarkTools"
+    "Pluto"
+    "Literate"
+    "Revise"
+    "Dagger"
+    "Unitful"
+    "ModelingToolkit"
+    "Cxx"
+    "Franklin"
+)
 
 for pkg in ${PACKAGES_TO_ANALYZE[@]}
 do
