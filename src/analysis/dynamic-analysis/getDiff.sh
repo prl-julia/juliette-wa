744d94ba43af5302ab093bf5fd2ce600bc7e716c
gelinasj <gelinas.j@husky.neu.edu>
2020-08-21 13:40:45 -0400

    generalizing getDiff

--- src/analysis/dynamic-analysis/getDiff.sh
+++ src/analysis/dynamic-analysis/getDiff.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-pkgName=IJulia-1.21.3
-stdioDir=package-data/$pkgName/stdio/
-filePath=$stdioDir/analysis-stderr.txt
-ltg=$(awk '/### DYNAMIC ANALYSIS LINE IDENTIFIER ###/ {print NR}' $filePath)
-tail -n +$(($ltg+1)) $filePath | sed '/Status/d' | sed '/analyzePkg/d' | sed '/testPkg/d'
+function getAfterLineId {
+    ltg=$(awk '/### DYNAMIC ANALYSIS LINE IDENTIFIER ###/ {print NR}' $1)
+    returnVal=$(tail -n +$(($ltg+1)) $1 | sed '/Status/d' | sed '/analyzePkg/d' | sed '/testPkg/d')
+}
+
+function getDiff {
+    getAfterLineId $1/analysis-$2.txt
+    analysis=$returnVal
+    getAfterLineId $1/test-$2.txt
+    test=$returnVal
+    returnVal=$(diff <(echo $analysis) <(echo $test))
+}
+
+pkgs=$(ls package-data-tst)
+for pkg in $pkgs
+do
+    stdioDir=package-data-tst/$pkg/stdio/
+    getDiff $stdioDir stdout
+    outDiff=$returnVal
+    getDiff $stdioDir stderr
+    errDiff=$returnVal
+    echo \#\#\# OUT DIFF \#\#\#
+    echo $outDiff
+    echo \#\#\# ERR DIFF \#\#\#
+    echo $errDiff
+done
